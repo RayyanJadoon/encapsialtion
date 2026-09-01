a3dea609ac467325f912fc2d137698aa5d2c376a
RayyanJadoon <jadoonkrayyan@gmail.com>
2026-09-01 19:27:38 +0100

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,9 @@
+class Point:
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+    def __str__(self):
+        return"{},{}".format(self.x, self.y)
+
+p = Point(3, 4)
+print(p)
